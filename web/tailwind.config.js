a092cb2809df26a37bb291d6800dd0b46ed86c05
pheralb <pablohdez4340@gmail.com>
2022-10-26 13:26:00 +0100

    ⚒️ Add fonts.

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
