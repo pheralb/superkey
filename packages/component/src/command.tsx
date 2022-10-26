dd056edec3302077e065538ebf17688564ef5739
pheralb <pablohdez4340@gmail.com>
2022-10-26 13:15:24 +0100

    ⚒️ Fix component style.

---
+++ packages/component/src/command.tsx
@@ -0,0 +1,9 @@
+export const Command = () => {
+  return (
+    <>
+      <div className="border border-red-300">
+        <p className="text-4xl text-bold">hola</p>
+      </div>
+    </>
+  );
+};
---
+++ packages/component/src/interfaces/commandInterface.ts
@@ -0,0 +1,3 @@
+export interface CommandInterface {
+  placeholder?: string;
+}
