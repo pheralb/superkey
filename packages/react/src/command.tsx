798b57a9c5384ffd0773446f4dac08d15a416273
pheralb <pablohdez4340@gmail.com>
2022-10-26 10:25:25 +0100

    ⚒️ Initial command component.

--- packages/react/src/command.tsx
+++ packages/react/src/command.tsx
@@ -2,9 +2,9 @@ export const Command = () => {
   return (
     <div className="rounded-md ">
       <a href="https://turborepo.org/docs/getting-started">
-        <div className="flex w-full items-center justify-center rounded-md border border-transparent bg-black px-8 py-3 text-base font-medium text-white no-underline hover:bg-gray-700 dark:bg-white dark:text-black dark:hover:bg-gray-300 md:py-3 md:px-10 md:text-lg md:leading-6">
+        <div className="flex items-center justify-center w-full px-8 py-3 text-base font-medium text-white no-underline bg-black border border-transparent rounded-md hover:bg-gray-700 dark:bg-white dark:text-black dark:hover:bg-gray-300 md:py-3 md:px-10 md:text-lg md:leading-6">
           Read the docs
-          <span className="ml-2 bg-gradient-to-r from-brandred to-brandblue bg-clip-text text-transparent">
+          <span className="ml-2 text-transparent bg-gradient-to-r from-brandred to-brandblue bg-clip-text">
             →
           </span>
         </div>
