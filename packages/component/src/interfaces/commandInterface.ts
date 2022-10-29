a201a6b0d31a369183b1127650cafca62652babd
pheralb <pablohdez4340@gmail.com>
2022-10-29 20:50:41 +0100

    ⚒️ Command interface improvements.

--- packages/component/src/interfaces/commandInterface.ts
+++ packages/component/src/interfaces/commandInterface.ts
@@ -1,3 +1,29 @@
-export interface CommandInterface {
+export interface CommandData {
+  id: number;
+  name: string;
+  description: string;
+  url?: string;
+}
+
+export interface CommandProps {
+  children: React.ReactNode;
+  open: boolean;
+  commandFunction?: (command: CommandData) => void;
+}
+
+export interface CommandInputProps {
   placeholder?: string;
+  onChange(event: React.ChangeEvent<HTMLInputElement>): void;
+  searchIcon?: React.ReactNode;
+  className?: string;
+}
+
+export interface CommandOptionProps {
+  value: string;
+  className?: string;
+  children: React.ReactNode;
+}
+
+export interface CommandListProps {
+  children: React.ReactNode;
 }
