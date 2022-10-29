1cdf2d93fe04877061c682472e9f1a86abcffdd1
pheralb <pablohdez4340@gmail.com>
2022-10-29 20:49:29 +0100

    ⚒️ Global command improvements.

--- packages/component/src/command.tsx
+++ packages/component/src/command.tsx
@@ -1,9 +1,25 @@
-export const Command = () => {
+import { useState } from "react";
+import { Dialog, Combobox } from "@headlessui/react";
+import { CommandProps } from "./interfaces/commandInterface";
+
+export const Command = (props: CommandProps) => {
+  const [open, setOpen] = useState(props.open);
   return (
-    <>
-      <div className="border border-red-300">
-        <p className="text-4xl text-bold">hola</p>
-      </div>
-    </>
+    <Dialog
+      open={open}
+      onClose={() => {
+        setOpen(false);
+      }}
+      className="fixed inset-0 p-5 pt-[25vh] overflow-y-auto"
+    >
+      <Dialog.Overlay className="fixed inset-0 bg-gray-300/75 opacity-30" />
+      <Combobox
+        as="div"
+        onChange={props.commandFunction}
+        className="relative max-w-xl mx-auto overflow-hidden rounded-md shadow-2xl ring-1 ring-black/5"
+      >
+        {props.children}
+      </Combobox>
+    </Dialog>
   );
 };
