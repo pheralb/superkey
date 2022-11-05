f970bb3530af5a3d0023169d05e48ec8dd48519c
pheralb <pablohdez4340@gmail.com>
2022-11-05 19:39:00 +0000

    ⚒️ Add next-seo.

---
+++ web/next-seo.config.ts
@@ -0,0 +1,50 @@
+export default {
+  title: "A stylized command menu for React",
+  titleTemplate: "%s - Superkey",
+  description: "A stylized command menu for React",
+  defaultTitle: "Superkey",
+  additionalLinkTags: [
+    {
+      rel: "icon",
+      href: "/img/favicon.ico",
+    },
+    {
+      rel: "apple-touch-icon",
+      href: "/img/apple-touch-icon-180x180.png",
+      sizes: "180x180",
+    },
+    {
+      rel: "apple-touch-icon",
+      href: "/img/apple-touch-icon-152x152.png",
+      sizes: "152x152",
+    },
+    {
+      rel: "apple-touch-icon",
+      href: "/img/apple-touch-icon-114x114.png",
+      sizes: "114x114",
+    },
+    {
+      rel: "manifest",
+      href: "/manifest.json",
+    },
+  ],
+  openGraph: {
+    site_name: "Superkey",
+    url: "https://superkey.vercel.app/",
+    type: "website",
+    locale: "en_IE",
+    images: [
+      {
+        url: "/images/og_image.jpg",
+        width: 1920,
+        height: 1080,
+        type: "image/jpg",
+      },
+    ],
+  },
+  twitter: {
+    handle: "@pheralb_",
+    site: "@pheralb_",
+    cardType: "summary_large_image",
+  },
+};
