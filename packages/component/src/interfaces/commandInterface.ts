b05758a4b92607bb84240b593a3a043708477192
pheralb <pablohdez4340@gmail.com>
2022-12-08 20:22:37 +0000

    ⚒️ Fix commandInterface.

--- packages/component/src/interfaces/commandInterface.ts
+++ packages/component/src/interfaces/commandInterface.ts
@@ -21,6 +21,7 @@ export interface CommandInputProps {
   placeholder?: string;
   onChange(event: React.ChangeEvent<HTMLInputElement>): void;
   searchIcon?: React.ReactNode;
+  className?: string;
   inputClassName?: string;
 }
 
